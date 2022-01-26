e2c8ebc284047e16a65f5e40cad9d1d52c45b069
Dmitry Klimov <auer.klimov@gmail.com>
2022-01-26 23:51:09 +0300

    Total statistics template

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import millify from 'millify'
-import { Col, Row, Typography, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
+import {
+    Row,
+    Col,
+    Statistic,
+    Typography,
+} from 'antd'
+
+const { Title } = Typography
 
 const Homepage = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Title level={2} className="heading">Итоговая крипто статистика</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total cryptocurrencies" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+            </Row>
+        </>
     );
 };
 
