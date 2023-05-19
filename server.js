468f4147b7bf21ff8229881d3643664e9abe8312
Dima Klimov <d.klimov@sptlab.ru>
2023-05-19 16:05:39 +0300

    Поправил порт на 3000

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
